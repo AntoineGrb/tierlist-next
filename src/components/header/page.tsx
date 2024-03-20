ae0ac2f9ced4d7a7d291d6c528dc656b4c18e039
unknown <antoine_grubert@hotmail.fr>
2024-03-20 16:20:32 +0100

    feat : handle desktop menu

--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -2,33 +2,41 @@
 
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
+import { useMediaQuery } from 'usehooks-ts';
 import { useMenu } from '@/src/context/menuContext';
 import { FaBars, FaTimes } from 'react-icons/fa';
+import Menu from '@/src/components/menu/page';
 
 const Header = () => {
 
     const { toggleMenu , isMenuMobileOpen } = useMenu()!;
+    const isDesktop = useMediaQuery('(min-width: 1024px)');
 
     //! Tester d'ajouter un icone de menu animé (cf. favoris Chrome)
 
     return (
     
         <header className='px-4 py-6 bg-black flex items-center justify-between border-b-2 border-b-white'>
-            <div className='flex items-center gap-2'>
-                <Image
-                    src='/assets/logo/logo test.png'
-                    alt='logo'
-                    width={30}
-                    height={30}
-                />
-                <h1 className='text-white'> S-TIER LIST </h1>
-            </div>
-            
-            {!isMenuMobileOpen ? (
-                <FaBars onClick={toggleMenu} className='text-white text-2xl justify-self-end' />
+            <Link href={'/'}>
+                <div className='flex items-center gap-2'>
+                    <Image src='/assets/logo/logo test.png' alt='logo' width={30} height={30}/>
+                    <h1 className='text-white'> S-TIER LIST </h1>
+                </div>
+            </Link>
+
+            {isDesktop ? (
+                <Menu />
             ) : (
-                <FaTimes onClick={toggleMenu} className='text-white text-2xl justify-self-end' />
+                <>
+                    {!isMenuMobileOpen ? (
+                        <FaBars onClick={toggleMenu} className='text-white text-2xl justify-self-end cursor-pointer' />
+                    ) : (
+                        <FaTimes onClick={toggleMenu} className='text-white text-2xl justify-self-end cursor-pointer' />
+                    )}
+                </>
             )}
+        
         </header>
     );
 };
--- src/components/menuMobile/page.tsx
+++ src/components/menuMobile/page.tsx
@@ -7,16 +7,17 @@ export default function MenuMobile() {
 
   const { isMenuMobileOpen } = useMenu()!;
 
-  //! Problème avec la transition d'opacité => le menu doit disparaitre completement sinon il est cliquable
-
     return (
-      <nav>
-          <ul className={`bg-black z-10 absolute w-full p-4 border-b border-white flex flex-col gap-3 transition-opacity duration-100 ease-out ${isMenuMobileOpen ? 'opacity-100' : 'opacity-0'}`}>
-              <li><Link href={'/'}>LOGIN</Link></li>
-              <li><Link href={'/'}>CREATE TEMPLATE </Link></li>
-              <li><Link href={'/'}>CATEGORIES</Link></li>
-              <li><Link href={'/'}>LAST TIER LISTS</Link> </li>
+
+      isMenuMobileOpen && (
+        <nav>
+          <ul className={`z-10 absolute w-full p-4 border-b border-white flex flex-col gap-3 transition-opacity duration-100 ease-out`}>
+              <li className='text-lg'> <Link href={'/'}>LOGIN</Link> </li>
+              <li className='text-lg'> <Link href={'/'}>CREATE TEMPLATE </Link> </li>
+              <li className='text-lg'> <Link href={'/'}>CATEGORIES</Link> </li>
+              <li className='text-lg'> <Link href={'/'}>LAST TIER LISTS</Link> </li>
           </ul>
       </nav>
+      )
     );
 }
