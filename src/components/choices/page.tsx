160cdd7bd0d9e1355e141285f589437de76189ac
unknown <antoine_grubert@hotmail.fr>
2024-03-20 15:14:52 +0100

    fix : handle choice div with wrap instead of scroll x

--- src/components/choices/page.tsx
+++ src/components/choices/page.tsx
@@ -16,16 +16,19 @@ const Choices = ({items}: ChoicesProps) => {
 
     return (
         <>
+        {/* <div className='fixed bottom-0 w-full whitespace-nowrap h-28 bg-gray-200 shadow-md overflow-x-scroll flex justify-start items-center gap-2 px-3 '>
+            {items.map((item, index) => (
+                <Image src={item.src} alt={item.name} width={60} height={60} />
+            ))}
+        </div>   */}
         <Droppable droppableId='choices' direction='horizontal'>
             {(provided) => (
-                <div ref={provided.innerRef} {...provided.droppableProps} className='fixed bottom-0 w-full h-28 bg-gray-200 shadow-md overflow-x-scroll flex justify-start items-center gap-2 px-3 '>
+                <div ref={provided.innerRef} {...provided.droppableProps} className='fixed bottom-0 w-full flex-wrap bg-gray-200 shadow-md overflow-x-scroll flex justify-start items-center gap-2 p-3 '>
                     {items.map((item, index) => (
                         <Draggable key={item.id} draggableId={item.id} index={index}>
                             {(provided) => (
                                 <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                                    <>
                                     <Image src={item.src} alt={item.name} width={60} height={60} />
-                                    </>
                                 </div>
                             )}
                         </Draggable>
