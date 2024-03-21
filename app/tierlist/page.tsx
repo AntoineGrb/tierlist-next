30528d24f17e469c2a22d3c876ccea31ef007da0
unknown <antoine_grubert@hotmail.fr>
2024-03-21 12:25:36 +0100

    fix : url data problem

--- app/tierlist/page.tsx
+++ app/tierlist/page.tsx
@@ -105,8 +105,8 @@ const TierList = () => {
 
             <main className="bg-black min-h-screen px-3 py-10">
                 <section className="pb-4 mb-4">
-                    <h2 className=" pb-4"> Programming Languages </h2>
-                    <p className=' text-justify'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam odit provident, at facere atque, rerum placeat modi distinctio illo culpa saepe dignissimos voluptates non voluptate, dolores quibusdam minus temporibus voluptatum quidem? Voluptates beatae similique, quia, possimus ad impedit esse velit molestiae molestias cupiditate, sint facilis quaerat alias repellendus amet adipisci.</p>
+                    <h2 className=" pb-4"> {list.title} </h2>
+                    <p className=' text-justify'> {list.description}</p>
                 </section>
 
                 <Board items={boardItems} isMobile={isMobile} />
--- src/data/lists/programming/list.ts
+++ src/data/lists/programming/list.ts
@@ -2,16 +2,16 @@ const list = {
     title: 'Programming languages',
     description: 'List of the most popular programming languages',
     items: [ 
-        { id: '1', name: 'c-plus', src:'/assets/test/langages/c_plus.png' },
-        { id: '2', name: 'c-sharp', src:'/assets/test/langages/c_sharp.jpg' },
-        { id: '3', name: 'c', src:'/assets/test/langages/c.png' },
-        { id: '4', name: 'java', src:'/assets/test/langages/java.png' },
-        { id: '5', name: 'javascript', src:'/assets/test/langages/javascript.png' },
-        { id: '6', name: 'php', src:'/assets/test/langages/php.png' },
-        { id: '7', name: 'python', src:'/assets/test/langages/python.png' },
-        { id: '8', name: 'ruby', src:'/assets/test/langages/ruby.png' },
-        { id: '9', name: 'swift', src:'/assets/test/langages/swift.png' },
-        { id: '10', name: 'typescript', src:'/assets/test/langages/typescript.png' },
+        { id: '1', name: 'c-plus', src:'/assets/items/programming/c_plus.png' },
+        { id: '2', name: 'c-sharp', src:'/assets/items/programming/c_sharp.jpg' },
+        { id: '3', name: 'c', src:'/assets/items/programming/c.png' },
+        { id: '4', name: 'java', src:'/assets/items/programming/java.png' },
+        { id: '5', name: 'javascript', src:'/assets/items/programming/javascript.png' },
+        { id: '6', name: 'php', src:'/assets/items/programming/php.png' },
+        { id: '7', name: 'python', src:'/assets/items/programming/python.png' },
+        { id: '8', name: 'ruby', src:'/assets/items/programming/ruby.png' },
+        { id: '9', name: 'swift', src:'/assets/items/programming/swift.png' },
+        { id: '10', name: 'typescript', src:'/assets/items/programming/typescript.png' },
     ]
 }
 
