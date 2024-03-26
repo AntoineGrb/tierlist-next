da40666132b20eb87173334a1570adb6e062584a
Antoine Grubert <119600392+AntoineGrb@users.noreply.github.com>
2024-03-26 14:28:28 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,10 +12,11 @@ export default async function Home() {
     <main className="bg-black min-h-screen px-3 py-8">
       <section className="pb-4">
         <h2 className="font-oswald text-3xl pb-4"> CREATE YOUR TIER LIST IN A FEW SECONDS </h2>
-        <p className=" text-justify"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam odit provident, at facere atque, rerum placeat modi distinctio illo culpa saepe dignissimos voluptates non voluptate, dolores quibusdam minus temporibus voluptatum quidem? Voluptates beatae similique, quia, possimus ad impedit esse velit molestiae molestias cupiditate, sint facilis quaerat alias repellendus amet adipisci.</p>
+        <p className=" text-justify"> Discover the ultimate tool for bringing your tier lists to life, all at your fingertips. Whether you're a fervent fan of gaming, movies, sports, or even programming languages, our platform empowers you to swiftly craft bespoke tier lists or utilize our vast library of templates. Dive into a world where your opinions are visualized in a compelling, organized manner. Perfect for enthusiasts eager to categorize their favorites or communities looking to debate the best and worst in any category. </p>
       </section>
       <CardsList title="Populars" lists={lists.filter(list => list.isPopular)} />
       <CardsList title="Sports" lists={lists.filter(list => list.category === 'Sports')} />
+      <CardsList title="Movies" lists={lists.filter(list => list.category === 'Movies')} />
       <CardsList title="Mangas" lists={lists.filter(list => list.category === 'Mangas')} />
       <CardsList title="Games" lists={lists.filter(list => list.category === 'Games')} />
     </main>
