1bd88f00fcb009134004a8e147348a6ff8e8eb8f
Antoine Grubert <119600392+AntoineGrb@users.noreply.github.com>
2024-03-27 15:47:30 +0100

    Update page.tsx

--- src/components/choices/page.tsx
+++ src/components/choices/page.tsx
@@ -11,7 +11,7 @@ const Choices = ({items, isMobile}: ChoicesProps) => {
         <>
         <Droppable droppableId='choices' direction='horizontal'>
             {(provided) => (
-                <div ref={provided.innerRef} {...provided.droppableProps} className='fixed bottom-0 w-full min-h-3 flex-wrap bg-gray-200 shadow-md overflow-x-scroll flex justify-start items-center gap-1 p-2 rounded-lg sm:relative sm:mt-1 sm:overflow-x-auto'>
+                <div ref={provided.innerRef} {...provided.droppableProps} className='fixed bottom-0 w-full min-h-12 flex-wrap bg-gray-200 shadow-md overflow-x-scroll flex justify-start items-center gap-1 p-2 rounded-lg sm:relative sm:mt-1 sm:overflow-x-auto'>
                     {items.map((item, index) => (
                         <Draggable key={item.id} draggableId={item.id.toString()} index={index}>
                             {(provided) => (
