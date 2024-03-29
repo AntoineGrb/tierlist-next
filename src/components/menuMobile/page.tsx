51a2acaa088066270047399afe14fa5e0d9e3043
unknown <antoine_grubert@hotmail.fr>
2024-03-29 08:56:15 +0100

    feat : close menuMobile when link is clicked

--- src/components/menuMobile/page.tsx
+++ src/components/menuMobile/page.tsx
@@ -5,19 +5,23 @@ import { useMenu } from '@/src/context/menuContext';
 
 export default function MenuMobile() {
 
-  const { isMenuMobileOpen } = useMenu()!;
-
-  /* Return le menu dans tous les cas et remplace le nav par le code suivant : 
-  <nav className={`absolute w-full z-10 bg-black p-4 border-b border-white flex flex-col gap-3
-      transition-all ease-in-out duration-500 ${isMenuMobileOpen ? "max-h-screen opacity-100" : "max-h-0 opacity-0"}`}> */
+  const { isMenuMobileOpen, toggleMenu } = useMenu()!;
 
     return (
       <nav className={`bg-black z-10 absolute w-full transition-all ease-in-out duration-500 overflow-hidden ${isMenuMobileOpen ? 'max-h-screen' : 'max-h-0'}`}>
           <ul className='p-4 border-b border-white flex flex-col gap-3'>
-            <li className='text-lg'> <Link href={'/'}>LOGIN</Link> </li>
-              <li className='text-lg'> <Link href={'/'}>TEMPLATES</Link> </li>
-              <li className='text-lg'> <Link href={'/new-template'}>CREATE TEMPLATE </Link> </li>
-              <li className='text-lg'> <Link href={'/'}>LAST USER LISTS</Link> </li>
+            <li className='text-lg'> 
+              <Link onClick={toggleMenu} href={'/'}>LOGIN</Link> 
+            </li>
+            <li className='text-lg'>
+               <Link onClick={toggleMenu} href={'/'}>TEMPLATES</Link> 
+            </li>
+            <li className='text-lg'>
+               <Link onClick={toggleMenu} href={'/new-template'}>CREATE TEMPLATE </Link> 
+            </li>
+            <li className='text-lg'>
+               <Link onClick={toggleMenu} href={'/'}>LAST USER LISTS</Link> 
+            </li>
           </ul>
       </nav>
     );
